b334c5589d5dd4dd14e1f442b217f341bba82663
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2016-08-04 18:08:38 +0900

    スクリプトの処理に関するコメントを追加

--- ddbj_validation/eval_after_dic_with_leejudges.pl
+++ ddbj_validation/eval_after_dic_with_leejudges.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/env perl
 
+=head
+バクテリアを対象としたENA→UniProt(Reviewed)辞書について李さんの評価を加えるスクリプト
+
+ENAをBefore、対応するUniProt ReviewedをAfterとする書換辞書（バクテリア）について、
+李さんの辞書に含まれないものを李さんにチェックして頂いた結果を $leejudge でオープン。
+EMBLデータに含まれているローカスタグを抽出したものを $locustag でオープン。
+上記のUniProt ReviewedとENAの対応を取得した結果を $tgd でオープン。
+=cut
+
 use warnings;
 use strict;
 use Fatal qw/open/;
@@ -63,6 +72,9 @@ while(<$tgd>){
     trim( $af );
     my @lct;
     next if $afters{$af};
+    # ENAのIDをキーにして対象DBエントリにLocus Tagが含まれているか確認し、
+    # 含まれている場合は、それがUniProt Reviewedのデフィニションに含まれるかを確認し、
+    # 含まれていたらそれを除去する。
     if( $eid2lct{$id} ){
 	for ( @{ $eid2lct{$id} } ){
 	    if(index($af, $_) > -1){
@@ -75,6 +87,10 @@ while(<$tgd>){
     }
     next if $afters{$af};
     $afters{$af}++;
+    # 書換後のデフィニションについてワード単位で李さん辞書に基くレベルを付与
+    # 0: 辞書から除外(locus_tag 類の混入)
+    # 1: gene 名などの略語類(辞書に追加)
+    # 2: 辞書に追加
     my %hash;
     my @levelseq;
     for (split / /, $af) {
