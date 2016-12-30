9eacd1aaf8a5044a413d0193148458f36ffc2e2e
Takatomo Fujisawa <tf@nig.ac.jp>
2016-12-31 05:12:38 +0900

    フッター部のアクセス時間のかかるイメージファイルを内部に配置し、レイアウト微修正

--- WebService/annotation.pl
+++ WebService/annotation.pl
@@ -385,8 +385,10 @@ $ curl -s http://togoannotator.dbcls.jp/ddbj_submission.txt | curl -s -F 'upload
 
     <p>
      <a rel="license" href="http://creativecommons.org/licenses/by/2.1/jp/">
-     <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/2.1/jp/88x31.png" /></a>
-       <a xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://purl.org/dc/dcmitype/Text" rel="dc:type" style="text-decoration:none;color:black">TogoAnnotator</a> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://dbcls.rois.ac.jp/" rel="cc:attributionURL">Database Center for Life Science (DBCLS)</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.1/jp/">Creative Commons &#34920;&#31034; 2.1 &#26085;&#26412; License</a>. This software includes the work that is distributed in the Apache License 2.0.
+     <img alt="Creative Commons License" style="border-width:0" src="/images/88x31.png" /></a>
+       <a xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://purl.org/dc/dcmitype/Text" rel="dc:type" style="text-decoration:none;color:black">TogoAnnotator</a> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://dbcls.rois.ac.jp/" rel="cc:attributionURL">Database Center for Life Science (DBCLS)</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.1/jp/">Creative Commons &#34920;&#31034; 2.1 &#26085;&#26412; License</a>.
+       </p>
+       <p>This software includes the work that is distributed in the Apache License 2.0.</p>
     </p>
     <!--//<p> &copy; Copyright 2016-2017 <a href="http://dbcls.rois.ac.jp/">DBCLS</a></p>-->
     </div>
