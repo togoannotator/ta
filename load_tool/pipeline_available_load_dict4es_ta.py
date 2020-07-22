ca67f0c0e4152524fe45e1ca1720b1d93dc1d8ff
acro-oshitari <59161693+oshitari@users.noreply.github.com>
2020-07-22 14:53:34 +0900

    辞書ロードファイルの新バージョンへの移行
    検索の遅延対応のため、ガイドライン遵守数を集計する方式のスクリプトに更新した

--- load_tool/pipeline_available_load_dict4es_ta.py
+++ load_tool/pipeline_available_load_dict4es_ta.py
@@ -115,75 +115,88 @@ class TsvElasticsearchConnector(object):
         guideline = {}
         guideline_compliance_list = []
         guideline_noncompliance_list = []
+        guideline_score_add = 0
 
         if self.check_american_spelling(text):
-            guideline["guideline_PN001"] = "1"
+            guideline["guideline_PN001"] = 1
             guideline_compliance_list.append("PN001")
+            guideline_score_add += guideline["guideline_PN001"]
         else:
             guideline_noncompliance_list.append("PN001")
 
         if self.check_diacritics(text):
-            guideline["guideline_PN003"] = "1"
+            guideline["guideline_PN003"] = 1
             guideline_compliance_list.append("PN003")
+            guideline_score_add += guideline["guideline_PN003"]
         else:
             guideline_noncompliance_list.append("PN003")
 
         if not check_freq.freqword_is_in(text): # 頻出語が含まれていなければガイドラインにマッチ
-            guideline["guideline_PN005"] = "1"
+            guideline["guideline_PN005"] = 1
             guideline_compliance_list.append("PN005")
+            guideline_score_add += guideline["guideline_PN005"]
         else:
             guideline_noncompliance_list.append("PN005")
 
         if not check_abbreviation.check_word_is_abbreviation(text): # 略語でなければガイドラインにマッチ
-            guideline["guideline_PN007"] = "1"
+            guideline["guideline_PN007"] = 1
             guideline_compliance_list.append("PN007")
+            guideline_score_add += guideline["guideline_PN007"]
         else:
             guideline_noncompliance_list.append("PN007")
 
         if self.check_prime(text): # プライムシンボルがない場合は加点
-            guideline["guideline_PN011"] = "1"
+            guideline["guideline_PN011"] = 1
             guideline_compliance_list.append("PN011")
+            guideline_score_add += guideline["guideline_PN011"]
         else:
             guideline_noncompliance_list.append("PN011")
 
         if not check_chemical_symbols.text_contains_symbols(text): # 含んでいなかったら加点
-            guideline["guideline_PN012"] = "1"
+            guideline["guideline_PN012"] = 1
             guideline_compliance_list.append("PN012")
+            guideline_score_add += guideline["guideline_PN012"]
         else:
             guideline_noncompliance_list.append("PN012")
 
 #        if not check_std_sci_abb.text_contains_std_sci_abb(text): # 含んでいたら非遵守 <- pipelineで実装済み
-#            guideline["guideline_PN013"] = "1"
+#            guideline["guideline_PN013"] = 1
 #            guideline_compliance_list.append("PN013")
+#            guideline_score_add += guideline["guideline_PN013"]
 #        else:
 #            guideline_noncompliance_list("PN013")
 
         if check_common_modifiers.text_has_hyphen_b4_cm_or_others(text): # Common Modifiersがあれば、その直前はハイフンが必要
-            guideline["guideline_PN016"] = "1"
+            guideline["guideline_PN016"] = 1
             guideline_compliance_list.append("PN016")
+            guideline_score_add += guideline["guideline_PN016"]
         else:
             guideline_noncompliance_list.append("PN016")
 
         if not check_colon_semicolon.text_contains_col_semi(text):
-            guideline["guideline_PN019"] = "1"
+            guideline["guideline_PN019"] = 1
             guideline_compliance_list.append("PN019")
+            guideline_score_add += guideline["guideline_PN019"]
         else:
             guideline_noncompliance_list.append("PN019")
 
         if self.check_greek(text): # Greek letterの使い方が不適切でない限り加点
-            guideline["guideline_PN027"] = "1"
+            guideline["guideline_PN027"] = 1
             guideline_compliance_list.append("PN027")
+            guideline_score_add += guideline["guideline_PN027"]
         else:
             guideline_noncompliance_list.append("PN027")
 
         if check_Delta.text_contains_Delta_steroid_fatty_acid(text):
-            guideline["guideline_PN028"] = "1"
+            guideline["guideline_PN028"] = 1
             guideline_compliance_list.append("PN028")
+            guideline_score_add += guideline["guideline_PN028"]
         else:
             guideline_noncompliance_list.append("PN028")
             
         guideline["guideline_compliance_list"] = guideline_compliance_list
         guideline["guideline_noncompliance_list"] = guideline_noncompliance_list
+        guideline["guideline_score_add"] = guideline_score_add
         return guideline
     
     # ドキュメント投入処理
